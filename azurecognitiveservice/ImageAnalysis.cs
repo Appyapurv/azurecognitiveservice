f3f8ecc3ca4c8ed2e200fe710271ca6cb9480da1
Apurv <aupadhyay@wellstreet.com>
2022-05-18 23:22:55 -0500

    feat: commit

--- azurecognitiveservice/ImageAnalysis.cs
+++ azurecognitiveservice/ImageAnalysis.cs
@@ -1,74 +1,60 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
-//using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
-//using System.Threading.Tasks;
-//using System.IO;
-//using Newtonsoft.Json;
-//using Newtonsoft.Json.Linq;
-//using System.Threading;
-//using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
 
-////namespace Azure
-////{
-//public class ImageAnalysis
-//{
-//    // Add your Computer Vision subscription key and endpoint
-//    static string subscriptionKey = "PASTE_YOUR_COMPUTER_VISION_SUBSCRIPTION_KEY_HERE";
-//    static string endpoint = "PASTE_YOUR_COMPUTER_VISION_ENDPOINT_HERE";
+namespace ComputerVisionQuickstart1
+{
+    public class ImageAnalysis
+    {
+        static string subscriptionKey = "7eb03718341c4d5397f185585d33aa4c";
+        static string endpoint = "https://sample23.cognitiveservices.azure.com/";
+        private const string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
 
-//    // URL image used for analyzing an image (image of puppy)
-//    private const string ANALYZE_URL_IMAGE = "https://moderatorsampleimages.blob.core.windows.net/samples/sample16.png";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Azure Cognitive Services Computer Vision");
+            Console.WriteLine();
+            ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
 
-//    static void Main(string[] args)
-//    {
-//        Console.WriteLine("Azure Cognitive Services Computer Vision - .NET quickstart example");
-//        Console.WriteLine();
+            AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
+        }
 
-//        // Create a client
-//        ComputerVisionClient client = Authenticate(endpoint, subscriptionKey);
+        public static ComputerVisionClient Authenticate(string endpoint, string key)
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
+              { Endpoint = endpoint };
+            return client;
+        }
 
-//        // Analyze an image to get features and other properties.
-//        AnalyzeImageUrl(client, ANALYZE_URL_IMAGE).Wait();
-//    }
+        public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
+        {
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("ANALYZE IMAGE - URL");
+            Console.WriteLine();
 
-//    /*
-//     * AUTHENTICATE
-//     * Creates a Computer Vision client used by each example.
-//     */
-//    public static ComputerVisionClient Authenticate(string endpoint, string key)
-//    {
-//        ComputerVisionClient client =
-//          new ComputerVisionClient(new ApiKeyServiceClientCredentials(key))
-//          { Endpoint = endpoint };
-//        return client;
-//    }
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Tags
+            };
 
-//    public static async Task AnalyzeImageUrl(ComputerVisionClient client, string imageUrl)
-//    {
-//        Console.WriteLine("----------------------------------------------------------");
-//        Console.WriteLine("ANALYZE IMAGE - URL");
-//        Console.WriteLine();
+            Console.WriteLine($"Analyzing the image {Path.GetFileName(imageUrl)}...");
+            Console.WriteLine();
+            var results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
 
-//        // Creating a list that defines the features to be extracted from the image. 
-
-//        List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
-//            {
-//                VisualFeatureTypes.Tags
-//            };
-
-//        Console.WriteLine($"Analyzing the image {Path.GetFileName(imageUrl)}...");
-//        Console.WriteLine();
-//        // Analyze the URL image 
-//        ImageAnalysis results = await client.AnalyzeImageAsync(imageUrl, visualFeatures: features);
-
-//        // Image tags and their confidence score
-//        Console.WriteLine("Tags:");
-//        foreach (var tag in results.Tags)
-//        {
-//            Console.WriteLine($"{tag.Name} {tag.Confidence}");
-//        }
-//        Console.WriteLine();
-//    }
-//}
-////}
+            Console.WriteLine("Tags:");
+            foreach (var tag in results.Tags)
+            {
+                Console.WriteLine($"{tag.Name} {tag.Confidence}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
